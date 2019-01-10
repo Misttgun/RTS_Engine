60f15e37b60392535e08d8ab56b6306d8c913c31
Misttgun <maurelarchange@gmail.com>
2019-01-10 19:59:28 +0100

    Début ECS

---
+++ include/ECS_Types.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef ECSTYPES_H
+#define ECSTYPES_H
+
+namespace Engine {
+	using ComponentType = unsigned int;
+#define N_COMPONENT_TYPES 32;
+
+	enum class Component {
+		Position = 0, SpriteSheet, State, Movable, Controller, Collidable
+	};
+
+	enum class System {
+		Renderer = 0, Movement, Collision, Control, State, SheetAnimation
+	};
+}
+
+#endif // !ECSTYPES_H
---
+++ include/TransformComponent.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef TRANSFORMCOMPONENT_H
+#define TRANSFORMCOMPONENT_H
+
+#include "BaseComponent.h"
+#include <SFML/System.hpp>
+#include <sstream>
+
+namespace Engine {
+	class TransformComponent: public BaseComponent
+	{
+	public:
+		TransformComponent();
+		~TransformComponent() = default;
+
+		void ReadIn(std::stringstream& t_stream);
+
+		void setPosition(const float t_x, const float t_y);
+		void setPosition(const sf::Vector2f& t_pos);
+		void setOrientation(const float t_up, const float t_right);
+		void setOrientation(const sf::Vector2i& t_orientation);
+		void setElevation(const float t_elevation);
+		const sf::Vector2f& getPosition() const { return m_position; }
+		const sf::Vector2i& getOrientation() const { return m_orientation; }
+		unsigned int getElevation() const { return m_elevation; }
+
+	private:
+		sf::Vector2f m_position;
+		sf::Vector2i m_orientation; //Orientation du sprite
+		unsigned int m_elevation;
+	};
+}
+
+#endif // !TRANSFORMCOMPONENT_H
